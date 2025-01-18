b744362708c186e22f142fe2ba3f35dd03ff0cee
kareemsalah720@gmail.com <kareemsalah720@gmail.com>
2025-01-18 21:35:50 +0200

    refactor to use case class

--- lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,15 +1,20 @@
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly/Features/home/domain/repos/home_repo.dart';
 import 'package:bookly/core/Errors/failure.dart';
+import 'package:bookly/core/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>,NoParam> {
   final HomeRepo homeRepo;
   FetchFeaturedBooksUseCase(this.homeRepo);
-
-  Future<Either<Failure, List<BookEntity>>> fatchFeaturedBooks() {
-    // check if there is internet
-    // check permissions
-    return homeRepo.fetchFeaturedBooks();
+  
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) {
+    // TODO: implement call
+    throw UnimplementedError();
   }
+  
+
+
+  
 }
