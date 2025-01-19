7338405f00fafae2aa3b9fc44cd4735786886585
kareemsalah720@gmail.com <kareemsalah720@gmail.com>
2025-01-19 04:35:20 +0200

    create home repo impl

--- lib/Features/home/data/data_sources/home_local_data_source.dart
+++ lib/Features/home/data/data_sources/home_local_data_source.dart
@@ -2,21 +2,27 @@ import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly/constants.dart';
 import 'package:hive/hive.dart';
 
-abstract class HomeLocalDataSource {
+abstract class  HomeLocalDataSource 
+{
+  
   List<BookEntity> fetchFeaturedBooks();
   List<BookEntity> fetchNewestBooks();
+
 }
 
-class HomeLocalDataSourceImpl implements HomeLocalDataSource {
+class HomeLocalDataSourceImpl extends HomeLocalDataSource
+{
   @override
-  List<BookEntity> fetchFeaturedBooks() {
-    var box = Hive.box<BookEntity>(kFeaturedBox);
-    return box.values.toList();
+  List<BookEntity> fetchNewestBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
   }
 
   @override
-  List<BookEntity> fetchNewestBooks() {
-    var box = Hive.box<BookEntity>(kNewestBox);
-    return box.values.toList();
+  List<BookEntity> fetchFeaturedBooks() {
+
+    var box = Hive.box<BookEntity>(kFeaturedBox);
+   return box.values.toList();
   }
+
 }
