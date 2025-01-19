b2b2fd970db06357225a8b42298387f9a98c747c
kareemsalah720@gmail.com <kareemsalah720@gmail.com>
2025-01-19 02:59:47 +0200

    Cache featured books

--- lib/Features/home/data/data_sources/home_remote_data_source.dart
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -1,5 +1,7 @@
 import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/functions/save_books_data.dart';
 import 'package:bookly/core/utils/service/api_service.dart';
 
 abstract class HomeRemoteDataSource {
@@ -15,6 +17,7 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
     var data = await apiService.get(
         endPoint: 'volumes?Filtering=free-ebooks&q=programming');
     List<BookEntity> books = getBooksList(data);
+    saveBooksData(books, kFeaturedBox);
     return books;
   }
 
