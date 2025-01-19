6dac2c3732a1ae5ca202859c6ca5a92561e07bdc
kareemsalah720@gmail.com <kareemsalah720@gmail.com>
2025-01-20 01:40:00 +0200

    create SimpleBlocObserver

---
+++ lib/core/utils/simple_bloc_observer.dart
@@ -0,0 +1,28 @@
+import 'dart:developer';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    // TODO: implement onEvent
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    // TODO: implement onError
+    super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    log(change.toString());
+    super.onChange(bloc, change);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    // TODO: implement onChange
+  }
+}
