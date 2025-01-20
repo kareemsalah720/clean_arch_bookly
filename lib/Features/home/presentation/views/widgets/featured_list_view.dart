085dd80c8e717396e798bac46545c38750033813
kareemsalah720@gmail.com <kareemsalah720@gmail.com>
2025-01-20 02:45:38 +0200

    Integrate fetch featured books cubit & Display featured books data

--- lib/Features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,20 +1,25 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_book_item.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({Key? key}) : super(key: key);
-
+  const FeaturedBooksListView({Key? key, required this.books})
+      : super(key: key);
+  final List<BookEntity> books;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * .3,
       child: ListView.builder(
+          itemCount: books.length,
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) {
-            return const Padding(
+            return Padding(
               padding: EdgeInsets.symmetric(horizontal: 8),
-              child: CustomBookImage(),
+              child: CustomBookImage(
+                imageUrl: books[index].image ?? '',
+              ),
             );
           }),
     );
